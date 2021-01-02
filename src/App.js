41121733b0ed9f05d3a98d70748a84fd857c63d0
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-02 16:40:02 -0500

    center game canvas on screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import { GameComponent } from "phaser-react-tools";
+import { Grid } from "semantic-ui-react";
 import React from "react";
 import config from "./config";
 
 export default function App() {
-  return <GameComponent config={config}></GameComponent>;
+  return (
+    <Grid style={{ height: "100vh", margin: 0 }} verticalAlign="middle">
+      <Grid.Row textAlign="center">
+        <Grid.Column>
+          <GameComponent config={config}></GameComponent>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
 }
