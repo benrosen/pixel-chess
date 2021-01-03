44e08a799bd7892390737d9a046b608a5f13ab9e
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-03 15:42:33 -0500

    create basic typedef for board object

---
+++ src/typedefs/RexBoard.js
@@ -0,0 +1,9 @@
+// TODO add methods to documentation
+
+/**
+ * Core object of Board system.
+ *
+ * @typedef RexBoard
+ * @property {number} width Board width in tiles.
+ * @property {number} height Board height in tiles.
+ */
