ccbbaca5c5b8b1f88573fd9ccd2df7e828e5b21d
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-03 15:23:24 -0500

    create typedefs for rex board objects

---
+++ src/typedefs/RexBoardConfig.js
@@ -0,0 +1,9 @@
+/**
+ * The config object for a board.
+ *
+ * @typedef RexBoardConfig
+ * @property {RexGridConfig} grid The grid config object.
+ * @property {number} width The width of the board.
+ * @property {number} height The height of the board.
+ * @see {module.boardConfig}
+ */
