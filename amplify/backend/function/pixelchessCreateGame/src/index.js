f5f8c3b5525f253eafe0d7dcbc94726431674203
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-06 01:43:50 -0500

    createGame lambda mutation resolver

---
+++ amplify/backend/function/pixelchessCreateGame/src/index.js
@@ -0,0 +1,37 @@
+/* Amplify Params - DO NOT EDIT
+	API_PIXELCHESS_GAMETABLE_ARN
+	API_PIXELCHESS_GAMETABLE_NAME
+	API_PIXELCHESS_GRAPHQLAPIIDOUTPUT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+const AWS = require("aws-sdk");
+
+const { createRecord, getEnvData } = require("aws-lambda-utility-layer");
+
+const GAME_TABLE_NAME = getEnvData(
+  process.env,
+  "API_PIXELCHESS_GAMETABLE_NAME"
+);
+
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+/**
+ * Create a game record in the DynamoDB table and return the result.
+ *
+ * @param {*} event The Lambda event to be handled.
+ */
+exports.handler = ({ id, identity, timestamp }) => {
+  const game = {
+    id: id,
+    moveText: "",
+    players: [identity.username],
+    status: "PENDING",
+    createdAt: timestamp,
+    updatedAt: timestamp,
+    __typename: "Game",
+  };
+  console.log(game);
+  return createRecord(documentClient, GAME_TABLE_NAME, game);
+};
