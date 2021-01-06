6a6d5f53c8d9c6cae70b7f5ebaca00944c7de811
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-05 22:13:55 -0500

    refactor schema to one type instead of three

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,252 +1,29 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const updateConnection = /* GraphQL */ `
-  mutation UpdateConnection(
-    $input: UpdateConnectionInput!
-    $condition: ModelConnectionConditionInput
-  ) {
-    updateConnection(input: $input, condition: $condition) {
+export const createGame = /* GraphQL */ `
+  mutation CreateGame {
+    createGame {
       id
-      matchId
       createdAt
       updatedAt
-      match {
-        id
-        connections {
-          nextToken
-        }
-        moves {
-          nextToken
-        }
-        playerIds
-        createdAt
-        updatedAt
-      }
+      version
+      moveText
+      players
+      status
     }
   }
 `;
-export const deleteConnection = /* GraphQL */ `
-  mutation DeleteConnection(
-    $input: DeleteConnectionInput!
-    $condition: ModelConnectionConditionInput
-  ) {
-    deleteConnection(input: $input, condition: $condition) {
+export const updateGame = /* GraphQL */ `
+  mutation UpdateGame($input: UpdateGameInput) {
+    updateGame(input: $input) {
       id
-      matchId
       createdAt
       updatedAt
-      match {
-        id
-        connections {
-          nextToken
-        }
-        moves {
-          nextToken
-        }
-        playerIds
-        createdAt
-        updatedAt
-      }
-    }
-  }
-`;
-export const updateMatch = /* GraphQL */ `
-  mutation UpdateMatch(
-    $input: UpdateMatchInput!
-    $condition: ModelMatchConditionInput
-  ) {
-    updateMatch(input: $input, condition: $condition) {
-      id
-      connections {
-        items {
-          id
-          matchId
-          createdAt
-          updatedAt
-        }
-        nextToken
-      }
-      moves {
-        items {
-          id
-          matchId
-          notation
-          createdAt
-          updatedAt
-        }
-        nextToken
-      }
-      playerIds
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const deleteMatch = /* GraphQL */ `
-  mutation DeleteMatch(
-    $input: DeleteMatchInput!
-    $condition: ModelMatchConditionInput
-  ) {
-    deleteMatch(input: $input, condition: $condition) {
-      id
-      connections {
-        items {
-          id
-          matchId
-          createdAt
-          updatedAt
-        }
-        nextToken
-      }
-      moves {
-        items {
-          id
-          matchId
-          notation
-          createdAt
-          updatedAt
-        }
-        nextToken
-      }
-      playerIds
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const updateMove = /* GraphQL */ `
-  mutation UpdateMove(
-    $input: UpdateMoveInput!
-    $condition: ModelMoveConditionInput
-  ) {
-    updateMove(input: $input, condition: $condition) {
-      id
-      matchId
-      notation
-      createdAt
-      updatedAt
-      match {
-        id
-        connections {
-          nextToken
-        }
-        moves {
-          nextToken
-        }
-        playerIds
-        createdAt
-        updatedAt
-      }
-    }
-  }
-`;
-export const deleteMove = /* GraphQL */ `
-  mutation DeleteMove(
-    $input: DeleteMoveInput!
-    $condition: ModelMoveConditionInput
-  ) {
-    deleteMove(input: $input, condition: $condition) {
-      id
-      matchId
-      notation
-      createdAt
-      updatedAt
-      match {
-        id
-        connections {
-          nextToken
-        }
-        moves {
-          nextToken
-        }
-        playerIds
-        createdAt
-        updatedAt
-      }
-    }
-  }
-`;
-export const createConnection = /* GraphQL */ `
-  mutation CreateConnection(
-    $input: CreateConnectionInput!
-    $condition: ModelConnectionConditionInput
-  ) {
-    createConnection(input: $input, condition: $condition) {
-      id
-      matchId
-      createdAt
-      updatedAt
-      match {
-        id
-        connections {
-          nextToken
-        }
-        moves {
-          nextToken
-        }
-        playerIds
-        createdAt
-        updatedAt
-      }
-    }
-  }
-`;
-export const createMatch = /* GraphQL */ `
-  mutation CreateMatch(
-    $input: CreateMatchInput!
-    $condition: ModelMatchConditionInput
-  ) {
-    createMatch(input: $input, condition: $condition) {
-      id
-      connections {
-        items {
-          id
-          matchId
-          createdAt
-          updatedAt
-        }
-        nextToken
-      }
-      moves {
-        items {
-          id
-          matchId
-          notation
-          createdAt
-          updatedAt
-        }
-        nextToken
-      }
-      playerIds
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const createMove = /* GraphQL */ `
-  mutation CreateMove(
-    $input: CreateMoveInput!
-    $condition: ModelMoveConditionInput
-  ) {
-    createMove(input: $input, condition: $condition) {
-      id
-      matchId
-      notation
-      createdAt
-      updatedAt
-      match {
-        id
-        connections {
-          nextToken
-        }
-        moves {
-          nextToken
-        }
-        playerIds
-        createdAt
-        updatedAt
-      }
+      version
+      moveText
+      players
+      status
     }
   }
 `;
