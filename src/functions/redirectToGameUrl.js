51fe41c526a69451dd7cf975688b6e36815b19d8
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-07 01:34:29 -0500

    redirect to game url inline instead of in dedicated function

--- src/functions/redirectToGameUrl.js
+++
@@ -1,8 +0,0 @@
-/**
- * Redirect the browser to the game url.
- *
- * @param {import("../typedefs/PixelChessGame").PixelChessGame} game The game to be redirected to.
- */
-export default function redirectToGameUrl(game) {
-  console.log(`redirect to /${game.id}`);
-}
--- src/functions/redirectToGameUrl.test.js
+++
@@ -1,7 +0,0 @@
-import redirectToGameUrl from "./redirectToGameUrl";
-
-describe("The redirectToGameUrl function", () => {
-  it("should be defined.", () => {
-    expect(redirectToGameUrl).toBeDefined();
-  });
-});
