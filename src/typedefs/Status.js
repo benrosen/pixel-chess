1a09e8069284d5a9db36cb16197e3eab16145658
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-07 01:05:51 -0500

    create typedefs for data stored in dynamodb

---
+++ src/typedefs/Status.js
@@ -0,0 +1,13 @@
+/**
+ * Possible values for the current status of the game.
+ *
+ * @readonly
+ * @enum {string}
+ */
+export default Status = {
+  PENDING: "PENDING",
+  UNDERWAY: "UNDERWAY",
+  DRAW: "DRAW",
+  BLACK_WON: "BLACK_WON",
+  WHITE_WON: "WHITE_WON",
+};
