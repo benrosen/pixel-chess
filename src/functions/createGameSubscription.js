95fa60492231dc8d895dd9070a73a01f8e4a29b6
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-07 22:50:03 -0500

    sketch framework for createGameSubscription function

---
+++ src/functions/createGameSubscription.js
@@ -0,0 +1,13 @@
+/**
+ * Subscribe to updates from a game.
+ *
+ * @param {*} game The game to subscribe to.
+ * @param {*} setGame The callback to update the game state.
+ * @returns {*} The newly created subscription.
+ */
+export default function createGameSubscription(game, setGame) {
+  // TODO return subscription to game or throw error
+}
+
+// TODO change jsdoc type of setGame to callback
+// TODO jsdoc typedef for return type (GameSubscription)
---
+++ src/functions/createGameSubscription.test.js
@@ -0,0 +1,7 @@
+import createGameSubscription from "./subscribeToGame";
+
+describe("The subscribeToGame function", () => {
+  it("should be defined.", () => {
+    expect(createGameSubscription).toBeDefined();
+  });
+});
