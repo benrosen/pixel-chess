ca75123c75e7192ac2ce540e59095537b6995797
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-07 22:46:11 -0500

    update subscription to require game id

--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -2,8 +2,8 @@
 // this is an auto generated file. This will be overwritten
 
 export const onUpdateGame = /* GraphQL */ `
-  subscription OnUpdateGame($owner: String) {
-    onUpdateGame(owner: $owner) {
+  subscription OnUpdateGame($id: ID!) {
+    onUpdateGame(id: $id) {
       id
       createdAt
       updatedAt
