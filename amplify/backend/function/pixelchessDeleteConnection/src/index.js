e9d5e8ee3064a172fbeebc3313f7f6d5730262b2
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-08 19:03:08 -0500

    sketch basic documentation for lambda functions

--- amplify/backend/function/pixelchessDeleteConnection/src/index.js
+++ amplify/backend/function/pixelchessDeleteConnection/src/index.js
@@ -1,15 +1,21 @@
-
-
+/**
+ * Delete a connection to a game.
+ *
+ * @param {*} event The Lambda event to handle.
+ * @returns The newly deleted connection.
+ */
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
+  // TODO implement deleteConnection
+  const response = {
+    statusCode: 200,
     //  Uncomment below to enable CORS requests
     //  headers: {
     //      "Access-Control-Allow-Origin": "*",
     //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+    //  },
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+  return response;
 };
+
+// TODO document with @throw tag
--- amplify/backend/function/pixelchessSetBlackPlayer/src/index.js
+++ amplify/backend/function/pixelchessSetBlackPlayer/src/index.js
@@ -1,15 +1,21 @@
-
-
+/**
+ * Set the black player of a game.
+ *
+ * @param {*} event The Lambda event to handle.
+ * @returns The newly updated game.
+ */
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
+  // TODO implement setBlackPlayer
+  const response = {
+    statusCode: 200,
     //  Uncomment below to enable CORS requests
     //  headers: {
     //      "Access-Control-Allow-Origin": "*",
     //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+    //  },
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+  return response;
 };
+
+// TODO document with @throw tag
