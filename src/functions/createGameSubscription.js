3dda17c105d350424dbe20e4e87e756bb0cc2d64
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-10 16:33:41 -0500

    throw not implemented error

--- src/functions/createGameSubscription.js
+++ src/functions/createGameSubscription.js
@@ -7,6 +7,7 @@
  */
 export default function createGameSubscription(game, setGame) {
   // TODO return subscription to game or throw error
+  throw new Error("Not implemented");
 }
 
 // TODO change jsdoc type of setGame to callback
