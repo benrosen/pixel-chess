2542c40327c16ae84a1d0200770565025784e2ba
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-10 14:14:22 -0500

    sketch getPlayers function

--- amplify/backend/function/pixelchessCreateConnection/src/getPlayers.js
+++ amplify/backend/function/pixelchessCreateConnection/src/getPlayers.js
@@ -4,6 +4,20 @@
  * @param {*} id The id of the game whose players you want to get.
  * @returns An object containing the black player's id and the white player's id.
  */
-module.exports = (id) => {
-  //
+module.exports = (id, lambda, graphQLFunctionName) => {
+  return invokeLambda(lambda, graphQLFunctionName, {
+    operation: /* GraphQL */ `
+      query GetGame($id: ID!) {
+        getGame(id: $id) {
+          black
+          white
+        }
+      }
+    `,
+    operationName: "getGame",
+    item: { id: id },
+  }).then((response) => {
+    console.log(response);
+    // TODO return response formatted as {black: "", white: ""}
+  });
 };
