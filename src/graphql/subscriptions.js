d263fe9040d11c7c2e9ac66b5f3a44b0fac9a93a
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-12 13:51:41 -0500

    rebuild api and schema.graphql

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onUpdateGame = /* GraphQL */ `
+  subscription OnUpdateGame($id: ID!) {
+    onUpdateGame(id: $id) {
+      id
+      black
+      moveText
+      status
+      white
+      createdAt
+      updatedAt
+      version
+    }
+  }
+`;
