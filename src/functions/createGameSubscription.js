10951f951eeda86471c1dda44164b8521ddede07
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-14 21:59:20 -0500

    subscribe to game updates

--- src/functions/createGameSubscription.js
+++ src/functions/createGameSubscription.js
@@ -1,14 +1,31 @@
+import { API } from "aws-amplify";
+
 /**
  * Subscribe to updates from a game.
  *
  * @param {*} game The game to subscribe to.
- * @param {*} setGame The callback to update the game state.
+ * @param {SetGameCallback} setGame The callback to update the game state.
  * @returns {*} The newly created subscription.
  */
 export default function createGameSubscription(game, setGame) {
-  // TODO return subscription to game or throw error
-  throw new Error("Not implemented");
+  return API.graphql({
+    query: /* GraphQL */ `
+      subscription OnUpdateGame($id: ID!) {
+        onUpdateGame(id: $id) {
+          id
+          moveText
+          status
+        }
+      }
+    `,
+    variables: { id: game.id },
+  }).subscribe({
+    next: (update) => {
+      setGame(update.value.data.onUpdateGame);
+    },
+    error: (error) => {
+      // TODO handle error
+      console.log(error);
+    },
+  });
 }
-
-// TODO change jsdoc type of setGame to callback
-// TODO jsdoc typedef for return type (GameSubscription)
