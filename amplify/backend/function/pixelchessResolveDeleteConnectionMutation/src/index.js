02a156168d5074934962c27107f6162aef08267b
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-21 22:07:49 -0500

    sketch framework for resolveDeleteConnectionMutation

--- amplify/backend/function/pixelchessResolveDeleteConnectionMutation/src/index.js
+++ amplify/backend/function/pixelchessResolveDeleteConnectionMutation/src/index.js
@@ -7,16 +7,26 @@
 	REGION
 Amplify Params - DO NOT EDIT */
 
+const AWS = require("aws-sdk");
+
+const { getEnvData, invokeLambda } = require("aws-lambda-utility-layer");
+
+const [EXECUTE_GRAPHQL_OPERATION, GAME_TABLE_NAME] = getEnvData(process.env, [
+  "FUNCTION_PIXELCHESSEXECUTEGRAPHQLOPERATION_NAME",
+  "API_PIXELCHESS_CONNECTIONTABLE_NAME",
+]);
+
+const documentClient = new AWS.DynamoDB.DocumentClient();
+const lambda = new AWS.Lambda();
+
+/**
+ * Delete your connection to a game.
+ *
+ * @param {DeleteConnectionEvent} event The Lambda event to handle.
+ * @returns The newly deleted connection.
+ */
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+  console.log(event);
+  // TODO delete record from dynamoDB table
+  // TODO ensure game status = BLACK_WON or status = WHITE_WON (executeGraphQLOperation updateStatus)
 };
---
+++ amplify/backend/function/pixelchessResolveDeleteConnectionMutation/typedefs/DeleteConnectionEvent.js
@@ -0,0 +1,4 @@
+/**
+ * @typedef DeleteConnectionEvent
+ *
+ */
